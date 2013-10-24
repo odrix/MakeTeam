1e0ae903b77d87f78158687b35b45fcbf23e1985
abey <adribey@free.fr>
2013-10-24 14:39:54 +0200

    code d'entrée et sorti des joueurs + changement de poste

--- withAngular/js/team.js
+++ withAngular/js/team.js
@@ -1,9 +1,10 @@
     function TeamCtrl($scope) {
-		var _players = [{nom: "Tom"},
-						{nom: "Harry"},
-						{nom: "Larry"},
-						{nom: "Luke"},
-						{nom: "Paul"},
+		var _players = [{nom: 'Tom'},
+						{nom: 'Harry'},
+						{nom: 'Larry'},
+						{nom: 'Luke'},
+						{nom: 'Roger'},
+						{nom: 'Paul'},
 						{nom: "Jean"},
 						{nom: 'Gille'},
 						{nom: 'Claude'}
@@ -17,40 +18,24 @@
 							playground: {
 								nbMaxPlayers: 11,
 								places: new Array(11),
-								addPlayer: function(player, no) {
+								goIn: function(no, player) {
 									if(no) {
-										player.no = no
 										this.places[no] = player
-										return 
 									}
-									if(player.no)
-										this.places[player.no] = player
 								},
-								removePlayer: function(player, no) {
+								goOut: function(no) {
 									if(no) {
 										this.places[no] = undefined
-										player.no = undefined
-										return
-									}
-									
-									if(player.no) {
-										this.places[player.no] = undefined
-										player.no = undefined
-										return
 									}
 								},
-								isAlreadyPlayer: function(player) {
-									if(player.no) {
-										return this.places[player.no] != undefined
+								getPlaceIndex: function(player) {
+									return this.places.indexOf(player)
+								},
+								getPlayer: function(no) {
+									if(no) {
+										return this.places[no]
 									}
-									
-									var result = false
-									var i=0
-									for(i=0;i<this.places.length;i++){
-										if(this.places[i] && this.places[i].no == player.no)
-											result = true
-									}
-									return result
+									return undefined
 								}}
 						}];
 		$scope.placesNo = [1,2,3,4,5,6,7,8,9,10,11];
@@ -61,35 +46,55 @@
 		// }, true);
 		 
 		 
-		$scope.placement = function(player, timeBox) {
-			if(player.no && player.no != '')
+		$scope.jouer = function(timebox, player, no) {
+			if(no && no != '')
 			{
-				var indexPlayer = timeBox.substitutes.indexOf(player)
+				// le joueur sort du banc
+				var indexPlayer = timebox.substitutes.indexOf(player)
 				if(indexPlayer>-1)
-					timeBox.substitutes.splice(indexPlayer, 1)
+					timebox.substitutes.splice(indexPlayer, 1)
 					
-				//timeBox.playground.removePlayer(player)
+				// si un joueur est déjà il est remplacé (va sur le banc)
+				var playerInPlace = timebox.playground.getPlayer(no)
+				if(playerInPlace)
+					$scope.reposer(timebox, playerInPlace)
 				
-				if(timeBox.playground.isAlreadyPlayer(player))
-					alert("y a déjà un joueur")
-				
-				timeBox.playground.addPlayer(player)
+				// pour aller sur le terrain
+				timebox.playground.goIn(no, player)
 			}
 		};
 		
-		$scope.changement = function(player, timeBox) {
-			if(player.no && player.no != '')
-			{					
-				var curPlace = player.no
-				var indexLastPlace = timeBox.playground.places.indexOf(player)
-				timeBox.substitutes.push(timeBox.playground.places[curPlace])
-				timeBox.playground.removePlayer(player, indexLastPlace)
-								
-				if(timeBox.playground.isAlreadyPlayer(player))
-					alert("y a déjà un joueur")
+		$scope.reposer = function(timebox, player) {
+			// le joueur sort du terrain
+			var index = timebox.playground.getPlaceIndex(player)
+			if(index > -1)
+				timebox.playground.goOut(index)
+			
+			// et il devient remplaçant
+			if(timebox.substitutes.indexOf(player) == -1)
+				timebox.substitutes.push(player)
+		}
+		
+		$scope.echanger = function(timebox, player, no) {
+			if(no)
+			{		
+				var index = timebox.playground.getPlaceIndex(player)			
+				timebox.playground.goOut(index)
 				
-				timeBox.playground.addPlayer(player, curPlace)
+				// si un joueur est déjà, ils échangent de place
+				var playerInPlace = timebox.playground.getPlayer(no)
+				if(playerInPlace)
+				{
+					//reposer(timebox, playerInPlace)
+					timebox.playground.goOut(no)
+					timebox.playground.goIn(index, playerInPlace)
+				}
+				
+				// le joueur prend sa place
+				timebox.playground.goIn(no, player)
 			}
 		};
+		
+		
 		
     }

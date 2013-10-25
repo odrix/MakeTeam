3a9f16fe6e4b941954570edccdc3ec48cf38a0be
adriw <adribey@free.Fr>
2013-10-26 00:06:15 +0200

    calcul du temps + remise en remplaçant

--- withAngular/js/team.js
+++ withAngular/js/team.js
@@ -19,11 +19,11 @@ App.controller('TeamCtrl', function($scope, $timeout) {
 		p.duration = 0
 		if($scope.timeBoxes) {			
 			for(var i=0;i<$scope.timeBoxes.length;i++) {
-				if($scope.timeBoxes[i].playground.places) {
-					for(var j=0;j<$scope.timeBoxes[i].playground.places.length;j++) {
-						for(var k=0;k<$scope.timeBoxes[i].playground.places[j].length;k++) {
-							var p = $scope.timeBoxes[i].playground.places[j][k]
-							if(p && p.player && p.id == p.player.id) {
+				if($scope.timeBoxes[i].places) {
+					for(var j=0;j<$scope.timeBoxes[i].places.length;j++) {
+						for(var k=0;k<$scope.timeBoxes[i].places[j].length;k++) {
+							var tmpp = $scope.timeBoxes[i].places[j][k]
+							if(tmpp && tmpp.player && tmpp.player.id == p.id) {
 								p.duration += $scope.timeBoxes[i].duration
 								break
 							}
@@ -42,117 +42,21 @@ App.controller('TeamCtrl', function($scope, $timeout) {
 	
 	$scope.timeBoxes = [{duration: maxTime,						
 						substitutes: angular.copy(_players),
-						playground: {
-							places: _places,
-							getPlace: function(no) {
-								for(var i=0;i<this.places.length;i++){
-									for(var j=0;j<this.places[i].length;j++) {
-										if(this.places[i][j].no == no) return this.places[i][j]
-									}
-								}
-							},
-							goIn: function(no, player) {
-								if(no) {
-									this.getPlace(no).player = player
-									//this.places[no] = player
-								}
-							},
-							goOut: function(no) {
-								if(no) {
-									this.getPlace(no).player = null
-								}
-							},
-							getPlaceIndex: function(player) {
-								for(var i=0;i<this.places.length;i++){
-									for(var j=0;j<this.places[i].length;j++) {
-										if(this.places[i][j].player && this.places[i][j].player == player) return this.places[i][j].no
-									}
-								}
-							},
-							getPlayer: function(no) {
-								if(no) {
-									return this.getPlace(no).player
-								}
-								return undefined
-							},
-							echanger: function(player, no) {
-								if(no){
-									var index = this.getPlaceIndex(player)			
-									this.goOut(index)
-									
-									// si un joueur est déjà, ils échangent de place
-									var playerInPlace = this.getPlayer(no)
-									if(playerInPlace) {
-										//this.reposer(playerInPlace)
-										this.goOut(no)
-										this.goIn(index, playerInPlace)
-									}
-									
-									// le joueur prend sa place
-									this.goIn(no, player)
-								}
-							}
-						},
-						jouer: function(player, no) {
-							if(no && no != '') {
-								// le joueur sort du banc
-								var indexPlayer = this.substitutes.indexOf(player)
-								if(indexPlayer>-1)
-									this.substitutes.splice(indexPlayer, 1)
-									
-								// si un joueur est déjà il est remplacé (va sur le banc)
-								var playerInPlace = this.playground.getPlayer(no)
-								if(playerInPlace)
-									this.reposer(playerInPlace)
-								
-								// pour aller sur le terrain
-								this.playground.goIn(no, player)
-								updatePlayerDuration(player)
-							}
-						},
-						reposer: function(player) {
-							// le joueur sort du terrain
-							var no = this.playground.getPlaceIndex(player)
-							if(no && no > -1)
-								this.playground.goOut(no)
-							
-							// et il devient remplaçant
-							if(this.substitutes.indexOf(player) == -1)
-								this.substitutes.push(player)
-							
-							updatePlayerDuration(player)
-						}
+						places: _places
 					}];
 	$scope.players = _players;
-	
-	// $scope.$watch('timeBoxes.substitutes', function(newValue) {
-		// alert("newValue:" + angular.toJson(newValue))
-	// }, true);
-	 
+		 
 
 	function updateTimeBoxesDuration() {
 		for(var i=0;i<$scope.timeBoxes.length;i++) {
 			$scope.timeBoxes[i].duration = maxTime / $scope.timeBoxes.length
 		}			
 	}
-
-	function updatePlayerDuration(player) {
-		for(var i=0;i<$scope.players.length;i++) {
-			if(player) {
-				if($scope.players[i].id == player.id) {
-					updateDuration($scope.players[i])
-					break;
-				}
-			} else {
-				updateDuration($scope.players[i])
-			}
-		}			
-	}		
 	
 	$scope.newTimeBox = function (timebox){
 		$scope.timeBoxes.push(angular.copy(timebox))
 		updateTimeBoxesDuration()
-		updatePlayerDuration()
+		$scope.updatePlayersDuration()
 	}
 	
 	$scope.deleteTimeBox = function(timebox) {
@@ -160,12 +64,14 @@ App.controller('TeamCtrl', function($scope, $timeout) {
 		if(indexTimebox>-1) {
 			$scope.timeBoxes.splice(indexTimebox, 1)
 			updateTimeBoxesDuration()
-			updatePlayerDuration()
+			$scope.updatePlayersDuration()
 		}
 	}
 	
-	$scope.dropped = function(event, ui) {
-		alert('dropped');
+	$scope.updatePlayersDuration = function(event, ui) {
+		for(var i=0;i<$scope.players.length;i++) {
+			updateDuration($scope.players[i])
+		}	
 	}
 	
 })
